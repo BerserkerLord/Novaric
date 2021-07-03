f6bbb8f18af6051205774c429cac8859df462e04
BerserkerLord <61397693+BerserkerLord@users.noreply.github.com>
2021-07-02 22:51:54 -0500

    Commit de todo

---
+++ admin/views/alert.php
@@ -0,0 +1,4 @@
+<div class="alert alert-<?php echo $resultado['status'] ?> alert-dismissible fade show" role="alert">
+    <?php echo $resultado['msg'] ?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
